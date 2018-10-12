40fe9349e7b637e1b01d5ef9454a4ca7aa79e2d9
HCherry <1242035@users.noreply.github.com>
2018-10-12 14:49:15 +0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,11 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('getConfig', function(req, res) {
+  var config = {
+      "appName": 'AppName',
+  };
+  res.success(config);
+});
+
