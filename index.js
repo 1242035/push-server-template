b1bd8abb4d244f00ac7cc099de2945f6cd5ff0ca
HCherry <1242035@users.noreply.github.com>
2018-10-12 11:07:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var config = require('./config').config;
 
 var api = new ParseServer(config.server);
 
-var dashboard = new ParseDashboard(config.dashboard);
+var options = { allowInsecureHTTP: true }
+var dashboard = new ParseDashboard(config.dashboard, options);
 
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
