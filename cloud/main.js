5b5af1b2ab46a7a92c59363f75e7dcf6f195f7db
HCherry <1242035@users.noreply.github.com>
2018-10-12 15:29:29 +0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,24 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    res.success('Hi');
 });
 
 Parse.Cloud.define('getConfig', function(req, res) {
-  var config = {
-      "appName": 'AppName',
-  };
-  return config;
+    var config = {
+        "appName": 'AppName',
+    };
+    return res.success(JSON.stringify(config));
 });
 
 Parse.Cloud.define('getFirebaseConfig', function(req, res) {
-  var config = process.env.FIREBASE_CONFIG_JS || {
-      apiKey: "apiKey",
-      authDomain: "authDomain",
-      databaseURL: "databaseURL",
-      projectId: "projectId",
-      storageBucket: "storageBucket",
-      messagingSenderId: "messagingSenderId"
-  };
-  return config;
+    var config = process.env.FIREBASE_CONFIG_JS || {
+        apiKey: "apiKey",
+        authDomain: "authDomain",
+        databaseURL: "databaseURL",
+        projectId: "projectId",
+        storageBucket: "storageBucket",
+        messagingSenderId: "messagingSenderId"
+    };
+    return res.success(JSON.stringify(config));
 });
 
